d4ef2bf6cf6e9b99a9e3d503843c1151828c48d4
akram56789 <akramhossain56789@gmail>
2022-04-15 16:03:54 +0600

    routin-added

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            {/* <h1>This is footer</h1> */}
+            <p><small>copyright ©️</small></p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/firebase.init.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+ 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDGyMLHgsxACOUfiRIBqI3XbiAf2uclG8",
+  authDomain: "genius-car-services-98ce4.firebaseapp.com",
+  projectId: "genius-car-services-98ce4",
+  storageBucket: "genius-car-services-98ce4.appspot.com",
+  messagingSenderId: "651206105700",
+  appId: "1:651206105700:web:912afe71e4582e9752dd71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
